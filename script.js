7b383e8944e9b5ae221609c43c2199dafa74f2e0
sarikabhatta <sarikabhatta5@gmail.com>
2024-06-03 21:57:49 +0545

    Add trigger to input value in search cities

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const form = document.getElementsByClassName("class-form")[0];
+  const input = document.getElementsByClassName("class-input")[0];
+
+  if (form && input) {
+    let isSubmitting = false;
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault(); // Prevent the default form submission
+      if (!isSubmitting) {
+        console.log(input.value); // Log the input value to the console
+      }
+      isSubmitting = false;
+    });
+
+    input.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        isSubmitting = true;
+        form.dispatchEvent(new Event("submit"));
+      }
+    });
+  } else {
+    console.error("Form or input element not found");
+  }
+});
